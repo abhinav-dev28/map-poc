62a0bc7d3a9eabcf9a0e8188c315f79429cd2ba2
abhinav-dev28 <abhinav.maurya@fitnearn.com>
2025-01-11 21:17:37 +0530

    chages in API

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,6 +1,6 @@
-const API =
-  "https://iwojpgsdff.execute-api.ap-south-1.amazonaws.com/dev/api/fitnearn/web/gym/map/";
-
+// const API =
+//   "https://iwojpgsdff.execute-api.ap-south-1.amazonaws.com/dev/api/fitnearn/web/gym/map/";  //UnComment this line and
+const API = "http://localhost:4000/api/"; //Comment out this line
 export const GET_SUGGESTION = API + "get-autocomplete";
 export const GET_PLACES = API + "get-places";
 export const GET_PLACE_DETAIL = API + "get-placedetail";
